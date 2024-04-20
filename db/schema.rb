8bbac30bfa3e7e9f0283b4a8ea839caf291562bb
Muhammad Rehan <rehanch642@gmail.com>
2024-04-20 21:19:23 +0500

    update user attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_17_154529) do
-  create_table "users", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2024_04_18_182704) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
+  enable_extension "plpgsql"
+
+  create_table "users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -19,6 +23,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_17_154529) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "phone_no"
+    t.string "farm_name"
+    t.string "province"
+    t.string "district"
+    t.string "tehsil"
+    t.text "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
