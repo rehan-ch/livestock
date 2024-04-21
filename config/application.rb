df33e075033e7412c89e3ecbacbfbd7d9ab742b9
Sajid Ali <web.expert106@gmail.com>
2024-04-21 05:28:30 +0500

    PRoduct Listing Product Show Sign In Sign Up Home Page

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Livestock
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.assets.paths << Rails.root.join('lib')
+    config.assets.paths << Rails.root.join('js')
 
     # Configuration for the application, engines, and railties goes here.
     #
