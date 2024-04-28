ae8095b73fd7d2079837f0852ecd90220600e86d
Sajid Ali <web.expert106@gmail.com>
2024-04-29 03:28:28 +0500

    Nav bar setting and contact

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_class(variable)
+    if controller.controller_name.downcase == variable.downcase
+      "active-menu"
+    else
+      ""
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:index, :show]
-  devise_for :users
+  resources :contacts, only: [:index]
+  resources :abouts, only: [:index]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
