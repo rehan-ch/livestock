fd1e8cd6b95f76d4e7f72071797b0711675a1d1c
Muhammad Rehan <rehanch642@gmail.com>
2024-05-07 20:16:51 +0500

    category update

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
     def index
         @categories = Category.all
         # update with popular scope
-        @popular_products = Product.first(20)
+        @popular_products = Product.approved.first(20)
     end
 end
