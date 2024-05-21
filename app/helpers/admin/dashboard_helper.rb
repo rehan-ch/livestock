53e0da6f7a9b3d60ec2a80592fc2d8d3d3797d56
Sajid Ali <web.expert106@gmail.com>
2024-05-21 23:54:04 +0500

    dashboard sidebar

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,5 +1,10 @@
 module Admin::DashboardHelper
     def active_tab(curr_tab)
-        params[:controller].split('/')[1] == curr_tab
+        "active" if controller.controller_name.downcase == curr_tab.downcase
+    end
+    def active_tab_with_tabs(curr_tab, tabs)
+        if (controller.controller_name.downcase == curr_tab.downcase) && (params[:type]&.downcase == tabs.downcase)
+            "active"
+        end
     end
 end
