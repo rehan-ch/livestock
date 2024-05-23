9e971d24881824746262ec74e7fb10ca8365a105
Muhammad Rehan <rehanch642@gmail.com>
2024-05-23 22:28:47 +0500

    update scopes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,12 +4,13 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.approved
-    @products = @products.filter_by_query(params[:q]) if params[:q].present?
-    @products = @products.filter_by_category(params[:category_id]) if params[:category_id].present?
-    @products = @products.filter_by_self_stock(params[:self_stock]) if params[:self_stock] == '1'
-    @products = @products.filter_by_price(params[:start_price], params[:end_price]) if params[:start_price].present? || params[:end_price].present?
-    @products = @products.filter_by_city(params[:city]) if params[:city].present?
-    @products =  @products.page(page).per(per(20))
+                       .filter_by_query(params[:q])
+                       .filter_by_category(params[:category_id])
+                       .filter_by_self_stock(params[:self_stock])
+                       .filter_by_price(params[:start_price], params[:end_price])
+                       .filter_by_city(params[:city])
+                       .page(params[:page])
+                       .per(20)
     @categories = Category.all
   end
 
