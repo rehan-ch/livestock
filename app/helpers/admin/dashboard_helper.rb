d132fa154c135bf54977446369682a3db0593bdf
RoR-webExpert106 <web.expert106@gmail.com>
2024-05-25 01:01:26 +0500

    worked on admin UI fixes and adjusments

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,11 +1,13 @@
 module Admin::DashboardHelper
-    def active_tab(curr_tab)
-        "active" if controller.controller_name.downcase == curr_tab.downcase
-    end
+  def active_tab(curr_tab)
+    "active" if controller.controller_name.downcase == curr_tab.downcase
+  end
 
-    def active_tab_with_tabs(curr_tab, tabs)
-        if (controller.controller_name.downcase == curr_tab.downcase) && (params[:type]&.downcase == tabs.downcase)
-            "active"
-        end
-    end
+  def active_tab_with_tabs(curr_tab, tabs)
+    "active"  if (controller.controller_name.downcase == curr_tab.downcase) && (params[:type]&.downcase == tabs.downcase)    
+  end
+
+  def dynamic_breadcrum_path
+    send("admin_#{controller_name}_path")
+  end
 end
