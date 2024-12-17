c1568aa70b104c9044370579f4313ced97c62b7f
Sajid Ali <web.expert106@gmail.com>
2024-12-18 04:42:08 +0500

    agent remove

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log, "#{release_path}/log/puma.access.log"
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
+set :ssh_options, { user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
@@ -42,7 +42,7 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "sudo systemctl  restart puma.service"
+      execute "systemctl  restart puma.service"
     end
   end
 
