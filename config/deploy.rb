a220ae8d0058bce3b726cadc9d04f4f13f3a5faf
Sajid Ali <web.expert106@gmail.com>
2024-12-18 05:00:34 +0500

    status

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ namespace :puma do
   desc 'Create Directories for Puma Pids and Socket'
   task :make_dirs do
     on roles(:app) do
-      execute "systemctl  restart puma.service"
+      execute "systemctl  status puma.service"
     end
   end
 
