915d1e8a90e4ca7761035b5add9b30807b4cbc7a
Sajid Ali <web.expert106@gmail.com>
2024-12-27 00:29:32 +0500

    credentials edit

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -3,7 +3,5 @@
 class Ckeditor::Asset < ActiveRecord::Base
   include Ckeditor::Orm::ActiveRecord::AssetBase
 
-  delegate :url, :current_path, :content_type, to: :data
-
-  validates :data, presence: true
+  has_one_attached :data
 end
