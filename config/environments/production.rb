e7a7ccc1cbecc712d68f3818ced6908e48f9b86f
Sajid Ali <web.expert106@gmail.com>
2024-12-27 04:42:30 +0500

    assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,13 +21,14 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  config.public_file_server.enabled = true
-
+  config.public_file_server.enabled = false
+  config.assets.compile = true
+  config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
   # Compress CSS using a preprocessor.
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
