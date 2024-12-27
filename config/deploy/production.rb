b0d1e074e3685beb84dfb4c0bb2f670f1a3523db
Sajid Ali <web.expert106@gmail.com>
2024-12-27 05:29:44 +0500

    production enviornment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-set :production
+set :development
 set :branch, 'main' # Replace 'main' with the branch you want to deploy
 server '103.173.63.62', user: 'livestock', roles: %w{app db web}
 
