6b4194a0cae65df5b3eecaf82386e256400b5a5f
Sajid Ali <web.expert106@gmail.com>
2025-05-26 21:42:58 +0500

    livestock pk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,12 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
 
+  # Set default URL options for Action Mailer
+  config.action_mailer.default_url_options = { 
+    host: 'livestock.pk',  # Your server IP
+    protocol: 'https'
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
