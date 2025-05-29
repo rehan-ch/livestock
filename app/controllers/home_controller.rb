5d548f6a93e4dc5781f9900467f80db6dd6fe774
Hamza Sohail <hamza.nexondev@gmail.com>
2025-05-29 15:55:20 +0500

    show only approved ads on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
         @categories = Category.where(parent: nil)
         # update with popular scope
         @popular_products = Product.approved.page(page).per(per)
-        @recent_products = Product.page(page).per(per)
+        @recent_products = Product.approved.page(page).per(per)
         @published_services = Service.published.page(page).per(per)
         @blogs = Blog.published.order("RANDOM()").page.per(per(3))
     end
