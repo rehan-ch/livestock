a878f33e7e180a99f13e656be644e70604065645
Hamza Sohail <hamza.nexondev@gmail.com>
2025-05-29 15:52:07 +0500

    Fixed blog image layout with random blogs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
         @popular_products = Product.approved.page(page).per(per)
         @recent_products = Product.page(page).per(per)
         @published_services = Service.published.page(page).per(per)
-        @blogs = Blog.published.page.per(per(3))
+        @blogs = Blog.published.order("RANDOM()").page.per(per(3))
     end
 
     def policy
